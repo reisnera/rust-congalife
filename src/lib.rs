0ea7cbdfb38eea3649bbd3ea8098f31a535131b5
Alex Reisner <thearcher@gmail.com>
2016-11-25 12:05:45 -0500

    Make Game struct thread-safe

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 extern crate rand;
 
 use self::rand::{thread_rng, Rng};
+use std::sync::{Arc, Mutex, RwLock};
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub enum State {
 	Dead,
 	Alive,
@@ -15,13 +16,15 @@ fn is_alive(state: &State) -> bool {
 	}
 }
 
+#[derive(Clone)]
 pub struct Game {
 	pub size: usize,
-	current: Vec<State>,
-	next: Vec<State>,
+	current: Arc<RwLock<Vec<State>>>,
+	next: Arc<Mutex<Vec<State>>>,
 }
 
 impl Game {
+
 	pub fn new(size: usize) -> Game {
 		let mut board: Vec<State> = vec![State::Dead; size * size];
 				
@@ -34,48 +37,59 @@ impl Game {
 
 		Game {
 			size: size,
-			current: board.clone(),
-			next: board.clone(),
+			current: Arc::new(RwLock::new(board.clone())),
+			next: Arc::new(Mutex::new(board.clone())),
 		}
 	}
 
-	pub fn get_current(&self) -> &[State] {
-		self.current.as_slice()
+	pub fn get_current_read_lock(&self) -> std::sync::RwLockReadGuard<Vec<State>> {
+		self.current.read().unwrap()
 	}
-}
 
-pub fn advance(game: &mut Game) {
-	for y in 0..game.size {
-		for x in 0..game.size {
-			let ul = if y >= 1 && x != 0 { game.current[(y - 1) * game.size + x - 1].clone()} else {State::Dead};
-			let uu = if y >= 1 {game.current[(y - 1) * game.size + x + 0].clone()} else {State::Dead};
-			let ur = if y >= 1 && x != game.size - 1 { game.current[(y - 1) * game.size + x + 1].clone()} else {State::Dead};
-			let l = if x != 0 {game.current[y * game.size + x - 1].clone()} else {State::Dead};
-			let r = if x != game.size - 1 {game.current[y * game.size + x + 1].clone()} else {State::Dead};
-			let dl = if y != game.size - 1 && x != 0 {game.current[(y + 1) * game.size + x - 1].clone()} else {State::Dead};
-			let dd = if y != game.size - 1 {game.current[(y + 1) * game.size + x + 0].clone()} else {State::Dead};
-			let dr = if y != game.size - 1 && x != game.size - 1 {game.current[(y + 1) * game.size + x + 1].clone()} else {State::Dead};
+	pub fn advance(&self) {
+		// Get read lock for 'current' and mutex lock for 'next'
+		// N.B. By locking 'next' first, this ensures that if another thread tries
+		// 		to call 'advance', that thread will block until this is done.
+		let mut next = self.next.lock().unwrap();
+		let current = self.current.read().unwrap();
 
-			let neighbors = vec![ul, uu, ur, l, r, dl, dd, dr].into_iter().filter(is_alive).count();
+		for y in 0..self.size {
+			for x in 0..self.size {
+				let ul = if y >= 1 && x != 0 { current[(y - 1) * self.size + x - 1].clone()} else {State::Dead};
+				let uu = if y >= 1 {current[(y - 1) * self.size + x + 0].clone()} else {State::Dead};
+				let ur = if y >= 1 && x != self.size - 1 { current[(y - 1) * self.size + x + 1].clone()} else {State::Dead};
+				let l = if x != 0 {current[y * self.size + x - 1].clone()} else {State::Dead};
+				let r = if x != self.size - 1 {current[y * self.size + x + 1].clone()} else {State::Dead};
+				let dl = if y != self.size - 1 && x != 0 {current[(y + 1) * self.size + x - 1].clone()} else {State::Dead};
+				let dd = if y != self.size - 1 {current[(y + 1) * self.size + x + 0].clone()} else {State::Dead};
+				let dr = if y != self.size - 1 && x != self.size - 1 {current[(y + 1) * self.size + x + 1].clone()} else {State::Dead};
 
-			game.next[y * game.size + x] = match game.current[y * game.size + x] {
-				State::Dead => {
-					if neighbors == 3 {
-						State::Alive
-					} else {
-						State::Dead
+				let neighbors = vec![ul, uu, ur, l, r, dl, dd, dr].into_iter().filter(is_alive).count();
+
+				next[y * self.size + x] = match current[y * self.size + x] {
+					State::Dead => {
+						if neighbors == 3 {
+							State::Alive
+						} else {
+							State::Dead
+						}
+					},
+					State::Alive => {
+						if neighbors < 2 || neighbors > 3 {
+							State::Dead
+						} else {
+							State::Alive
+						}
 					}
-				},
-				State::Alive => {
-					if neighbors < 2 || neighbors > 3 {
-						State::Dead
-					} else {
-						State::Alive
-					}
-				}
-			};
+				};
+			}
 		}
+
+		// Exchange read lock for write lock
+		drop(current);
+		let mut current = self.current.write().unwrap();
+
+		*current = next.clone();
 	}
 
-	game.current.clone_from(&game.next);
 }
