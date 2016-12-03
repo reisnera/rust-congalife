a75ff669e1041d6ab1abba1ae175ed4f4631d5f3
Alex Reisner <thearcher@gmail.com>
2016-12-03 12:09:33 -0500

    Add some larger advancing benchmarks
    - Comment out toroidal benches as this is not implemented for now

--- benches/advance_game.rs
+++ benches/advance_game.rs
@@ -26,19 +26,31 @@ fn bench_advance_game_512(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_advance_game_toroidally_64(b: &mut Bencher) {
-	let game: Game = Game::new(64, 0.2);
-    b.iter(|| game.advance_toroidally());
+fn bench_advance_game_1024(b: &mut Bencher) {
+    let game: Game = Game::new(1024, 0.2);
+    b.iter(|| game.advance());
 }
 
 #[bench]
-fn bench_advance_game_toroidally_128(b: &mut Bencher) {
-    let game: Game = Game::new(128, 0.2);
-    b.iter(|| game.advance_toroidally());
+fn bench_advance_game_2048(b: &mut Bencher) {
+    let game: Game = Game::new(2048, 0.2);
+    b.iter(|| game.advance());
 }
 
-#[bench]
-fn bench_advance_game_toroidally_512(b: &mut Bencher) {
-    let game: Game = Game::new(512, 0.2);
-    b.iter(|| game.advance_toroidally());
-}
+// #[bench]
+// fn bench_advance_game_toroidally_64(b: &mut Bencher) {
+// 	let game: Game = Game::new(64, 0.2);
+//     b.iter(|| game.advance_toroidally());
+// }
+
+// #[bench]
+// fn bench_advance_game_toroidally_128(b: &mut Bencher) {
+//     let game: Game = Game::new(128, 0.2);
+//     b.iter(|| game.advance_toroidally());
+// }
+
+// #[bench]
+// fn bench_advance_game_toroidally_512(b: &mut Bencher) {
+//     let game: Game = Game::new(512, 0.2);
+//     b.iter(|| game.advance_toroidally());
+// }
