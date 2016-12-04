0ccfca492b9712171ac63b80f182973ab24441b9
Alex Reisner <thearcher@gmail.com>
2016-12-03 23:45:57 -0500

    Optimize performance
    - Get rid of GameCell
    - Game struct now has additional Vec<Coord> to store coordinate data
    - 'current' is now Vec<bool>
    - Significant performance increase for larger game sizes!
    - Unfortunately can not keep new game generation parallelized
    - Also create size() getter and make size struct member private

--- src/lib.rs
+++ src/lib.rs
@@ -2,57 +2,21 @@ extern crate rand;
 extern crate rayon;
 // extern crate crossbeam;
 
+use self::rand::Rng;
 use std::sync::{Arc, Mutex, RwLock};
 use rayon::prelude::*;
 
-#[derive(Clone, PartialEq, Eq)]
-pub struct GameCell {
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct Coord {
 	x: usize,
 	y: usize,
-	pub alive: bool,
-}
-
-impl GameCell {
-	fn count_neighbors(&self, board: &Vec<GameCell>, size: usize) -> u8 {
-		let ul = if self.y >= 1 && self.x != 0 { board[(self.y - 1) * size + self.x - 1].alive } else { false };
-		let uu = if self.y >= 1 { board[(self.y - 1) * size + self.x + 0].alive } else { false };
-		let ur = if self.y >= 1 && self.x != size - 1 { board[(self.y - 1) * size + self.x + 1].alive } else { false };
-		let l  = if self.x != 0 { board[self.y * size + self.x - 1].alive } else { false };
-		let r  = if self.x != size - 1 { board[self.y * size + self.x + 1].alive } else { false };
-		let dl = if self.y != size - 1 && self.x != 0 { board[(self.y + 1) * size + self.x - 1].alive } else { false };
-		let dd = if self.y != size - 1 { board[(self.y + 1) * size + self.x + 0].alive } else { false };
-		let dr = if self.y != size - 1 && self.x != size - 1 { board[(self.y + 1) * size + self.x + 1].alive } else { false };
-
-		let mut neighbors: u8 = 0;
-		if ul == true { neighbors += 1; }
-		if uu == true { neighbors += 1; }
-		if ur == true { neighbors += 1; }
-		if l  == true { neighbors += 1; }
-		if r  == true { neighbors += 1; }
-		if dl == true { neighbors += 1; }
-		if dd == true { neighbors += 1; }
-		if dr == true { neighbors += 1; }
-
-		neighbors
-	}
-
-	// Calculates the next state for this cell (but does not set it!)
-	fn calculate_next_state_from_neighbor_count(&self, number_of_neighbors: u8) -> bool {
-		match self.alive {
-			false => {
-				if number_of_neighbors != 3 { false } else { true }
-			},
-			true => {
-				if number_of_neighbors < 2 || number_of_neighbors > 3 { false } else { true }
-			},
-		}
-	}
 }
 
 #[derive(Clone)]
 pub struct Game {
-	pub size: usize,
-	current: Arc<RwLock< Vec<GameCell> >>,
+	size: usize,
+	coords: Arc< Vec<Coord> >,
+	current: Arc<RwLock< Vec<bool> >>,
 	next: Arc<Mutex< Vec<bool> >>,
 }
 
@@ -70,32 +34,38 @@ pub struct Game {
 // }
 
 impl Game {
-	pub fn new(size: usize, percent_chance_for_cell_to_be_alive: f64) -> Game {
+	pub fn new(size: usize, percent_chance_for_cell_to_be_alive: f32) -> Game {
 		assert!(size >= 3);
 		assert!(percent_chance_for_cell_to_be_alive < 1.0);
-		assert!(percent_chance_for_cell_to_be_alive >= 0.0);
+		assert!(percent_chance_for_cell_to_be_alive > 0.0);
 
-		let mut board = vec![GameCell {x: 0, y: 0, alive: false}; size*size];
+		let mut coords = vec![Coord {x: 0, y: 0}; size*size];
+		let mut board  = vec![false; size*size];
 
-		board.par_iter_mut()
-			.enumerate()
-			.for_each(| (i, game_cell) | {
-				game_cell.y = i/size;
-				game_cell.x = i - game_cell.y * size;
-				game_cell.alive = match rand::random::<f64>() {
-					i if i < percent_chance_for_cell_to_be_alive => true,
-					_ => false,
-				};
-			});
+		let mut rng = rand::thread_rng(); // Cache thread-local rng
+
+		for (i, state) in board.iter_mut().enumerate() {
+			coords[i].y = i/size;
+			coords[i].x = i - coords[i].y * size;
+			*state = match rng.next_f32() {
+				i if i < percent_chance_for_cell_to_be_alive => true,
+				_ => false,
+			};
+		}
 
 		Game {
 			size: size,
+			coords: Arc::new(coords),
 			current: Arc::new(RwLock::new(board)),
 			next: Arc::new(Mutex::new(Vec::with_capacity(size))),
 		}
 	}
 
-	pub fn get_current_read_lock(&self) -> std::sync::RwLockReadGuard< Vec<GameCell> > {
+	pub fn size(&self) -> usize {
+		self.size
+	}
+
+	pub fn get_current_read_lock(&self) -> std::sync::RwLockReadGuard< Vec<bool> > {
 		self.current.read().unwrap()
 	}
 
@@ -103,10 +73,11 @@ impl Game {
 		let mut next_guard = self.next.lock().unwrap();
 		let current_guard = self.current.read().unwrap();
 
-		current_guard.par_iter()
-			.map(|game_cell| {
-				let neighbor_count = game_cell.count_neighbors(&*current_guard, self.size);
-				game_cell.calculate_next_state_from_neighbor_count(neighbor_count)
+		self.coords.par_iter()
+			.enumerate()
+			.map(| (i, coord) | {
+				let neighbor_count = self.count_neighbors(coord, &*current_guard);
+				Game::calculate_next_state(current_guard[i], neighbor_count)
 			})
 			.collect_into(&mut *next_guard);
 
@@ -116,9 +87,44 @@ impl Game {
 
 		current_guard.par_iter_mut()
 			.enumerate()
-			.for_each(| (i, game_cell) | {
-				game_cell.alive = next_guard[i];
+			.for_each(| (i, state) | {
+				*state = next_guard[i];
 			});
+	}
+
+	fn count_neighbors(&self, coord: &Coord, board: &Vec<bool>) -> u8 {
+		let ul = if coord.y >= 1 && coord.x != 0 { board[(coord.y - 1) * self.size + coord.x - 1] } else { false };
+		let uu = if coord.y >= 1 { board[(coord.y - 1) * self.size + coord.x + 0] } else { false };
+		let ur = if coord.y >= 1 && coord.x != self.size - 1 { board[(coord.y - 1) * self.size + coord.x + 1] } else { false };
+		let l  = if coord.x != 0 { board[coord.y * self.size + coord.x - 1] } else { false };
+		let r  = if coord.x != self.size - 1 { board[coord.y * self.size + coord.x + 1] } else { false };
+		let dl = if coord.y != self.size - 1 && coord.x != 0 { board[(coord.y + 1) * self.size + coord.x - 1] } else { false };
+		let dd = if coord.y != self.size - 1 { board[(coord.y + 1) * self.size + coord.x + 0] } else { false };
+		let dr = if coord.y != self.size - 1 && coord.x != self.size - 1 { board[(coord.y + 1) * self.size + coord.x + 1] } else { false };
+
+		let mut neighbors: u8 = 0;
+		if ul == true { neighbors += 1; }
+		if uu == true { neighbors += 1; }
+		if ur == true { neighbors += 1; }
+		if l  == true { neighbors += 1; }
+		if r  == true { neighbors += 1; }
+		if dl == true { neighbors += 1; }
+		if dd == true { neighbors += 1; }
+		if dr == true { neighbors += 1; }
+
+		neighbors
+	}
+
+	// Calculates the next state for this cell (but does not set it!)
+	fn calculate_next_state(state: bool, number_of_neighbors: u8) -> bool {
+		match state {
+			false => {
+				if number_of_neighbors != 3 { false } else { true }
+			},
+			true => {
+				if number_of_neighbors < 2 || number_of_neighbors > 3 { false } else { true }
+			},
+		}
 	}
 
 	// pub fn advance_toroidally(&self) {
