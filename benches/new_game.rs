4f86ac5af5ab3b6341c0de3e48155752fc95b1c7
Alex Reisner <thearcher@gmail.com>
2016-12-03 21:11:22 -0500

    Fix typo and add larger new game benchmark

--- benches/new_game.rs
+++ benches/new_game.rs
@@ -19,5 +19,10 @@ fn bench_new_game_128(b: &mut Bencher) {
 
 #[bench]
 fn bench_new_game_512(b: &mut Bencher) {
+    b.iter(|| Game::new(512, 0.2));
+}
+
+#[bench]
+fn bench_new_game_1024(b: &mut Bencher) {
     b.iter(|| Game::new(1024, 0.2));
 }
